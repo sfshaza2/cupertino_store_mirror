7d1b0422d9f777c5a36c1c2fe23cb221df093593
Tim Sneath <tim@sneath.org>
2019-04-19 20:25:31 -0700

    added a comma

--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -15,31 +15,32 @@ class SearchBar extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
-          child: Row(
-            children: <Widget>[
-              const Icon(
-                CupertinoIcons.search,
+        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+        child: Row(
+          children: <Widget>[
+            const Icon(
+              CupertinoIcons.search,
+              color: Styles.searchIconColor,
+            ),
+            Expanded(
+              child: CupertinoTextField(
+                controller: controller,
+                focusNode: focusNode,
+                style: Styles.searchText,
+                cursorColor: Styles.searchCursorColor,
+                placeholder: 'Search',
+              ),
+            ),
+            GestureDetector(
+              child: const Icon(
+                CupertinoIcons.clear_thick_circled,
                 color: Styles.searchIconColor,
               ),
-              Expanded(
-                child: CupertinoTextField(
-                  controller: controller,
-                  focusNode: focusNode,
-                  style: Styles.searchText,
-                  cursorColor: Styles.searchCursorColor,
-                  placeholder: 'Search',
-                ),
-              ),
-              GestureDetector(
-                child: const Icon(
-                  CupertinoIcons.clear_thick_circled,
-                  color: Styles.searchIconColor,
-                ),
-                onTap: controller.clear,
-              ),
-            ],
-          )),
+              onTap: controller.clear,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
